004b3e34b53bcd2daba80b7dc5b5039ab49f688e
hoodielive <hoodielive@gmail.com>
2020-12-08 20:20:39 -0500

    no time for this lol

--- main.kt
+++ main.kt
@@ -1,3 +1,13 @@
+import java.io.File
+
+// Get the passed in path, i.e. "-d some/path" or use the current path.
+
+val path = if (args.contains("-d")) args[1 + args.indexOf("-d")]
+           else "."
+
+val folders = File(path).listFiles { file -> file.isDirectory() }
+folders?.forEach { folder -> println(folder) }
+
 fun main()
 {
 	println("Hello, Aeon")
